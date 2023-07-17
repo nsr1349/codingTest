e0e4c0b4aa646754c32c3931276ec000ce655392
nsr1349 <79898508+nsr1349@users.noreply.github.com>
2023-07-17 14:15:56 +0900

    문제풀

---
+++ 프로그래머스/N개의_최소공배수.js
@@ -0,0 +1,13 @@
+function solution(arr) {
+  var answer = arr[0];
+  for (let i = 1 ; i < arr.length; i++){
+      answer = 최소공배수(answer, arr[i])
+  }
+  return answer
+  
+  function 최소공배수 ( a, b ){
+      for (let i = Math.max(a,b) ; i < (a*b) + 1 ; i ++){   
+          if ( i % a === 0 && i % b === 0 ) return i
+      }
+  }
+}
