86c3c4167c3f4cd928fdb4a40123cee1ea82e2a8
nsr1349 <79898508+nsr1349@users.noreply.github.com>
2024-09-16 19:05:20 +0900

    해결

---
+++ 백준/공 넣기.js
@@ -0,0 +1,13 @@
+const input = require('fs').readFileSync(0, 'utf-8').toString().trim().split("\n");
+
+const bucket = {}
+const bucketLen = +input[0].split(' ')[0]
+
+for (let i = 0; i <  bucketLen; i++)bucket[i + 1] = 0
+
+for (let i = 1; i <  input.length; i++){
+    const [start , end, ball] = input[i].split(' ').map((a)=> +a)
+    for (let j = start; j <  end + 1; j++) bucket[j] = ball
+}
+
+console.log(Object.values(bucket).join(' '))
