571ef5a1f66798d12742f77fc1acb784ed5eefbd
nsr1349 <79898508+nsr1349@users.noreply.github.com>
2024-09-17 22:36:03 +0900

    해결

--- 백준/공 넣기.js
+++ 백준/공 넣기.js
@@ -4,7 +4,7 @@ const bucket = {}
 const bucketLen = +input[0].split(' ')[0]
 
 for (let i = 0; i <  bucketLen; i++)bucket[i + 1] = 0
-
+console.log(bucket)
 for (let i = 1; i <  input.length; i++){
     const [start , end, ball] = input[i].split(' ').map((a)=> +a)
     for (let j = start; j <  end + 1; j++) bucket[j] = ball
